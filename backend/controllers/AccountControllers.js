cb292c2aba5b8b6977e890b6629bd63c55481696
saikiranAnnam <saikiranannam23@gmail.com>
2024-02-07 21:40:24 +0530

    api/v1/account/transfer

--- backend/controllers/AccountControllers.js
+++ backend/controllers/AccountControllers.js
@@ -1,3 +1,4 @@
+const { mongoose } = require("mongoose");
 const Account = require("../models/account");
 
 const getBalance = async (req, res) => {
@@ -10,6 +11,53 @@ const getBalance = async (req, res) => {
   });
 };
 
+const transferFund = async (req, res) => {
+  const session = await mongoose.startSession();
+
+  session.startTransaction();
+  const { amount, to } = req.body;
+
+  // Fetch the accounts within the transaction
+  const account = await Account.findOne({ userId: req.userId }).session(
+    session
+  );
+
+  if (!account || account.balance < amount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Insufficient balance",
+    });
+  }
+
+  const toAccount = await Account.findOne({ userId: to }).session(session);
+
+  if (!toAccount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Invalid account",
+    });
+  }
+
+  // Perform the transfer
+  await Account.updateOne(
+    { userId: req.userId },
+    { $inc: { balance: -amount } }
+  ).session(session);
+
+  await Account.updateOne(
+    { userId: to },
+    { $inc: { balance: amount } }
+  ).session(session);
+
+  // Commit the transaction
+  await session.commitTransaction();
+
+  res.json({
+    message: "Transfer successful",
+  });
+};
+
 module.exports = {
   getBalance,
+  transferFund,
 };
--- backend/routers/account.js
+++ backend/routers/account.js
@@ -3,6 +3,8 @@ const authMiddleware = require("../middlewares/userAuth");
 const router = express.Router();
 const AccountController = require("../controllers/AccountControllers");
 
-router.get("/balance", authMiddleware, AccountController.getBalance);
+router
+  .get("/balance", authMiddleware, AccountController.getBalance)
+  .post("/transfer", authMiddleware, AccountController.transferFund);
 
 module.exports = { router };
